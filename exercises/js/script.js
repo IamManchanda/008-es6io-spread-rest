c9b07fd5a83249b7393b878d443acc2ef9a47035
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-08 12:19:35 +0530

    Spread and spread exercises!

---
+++ exercises/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * JS
+ */
+
+const heading = document.querySelector('.jump');
+heading.innerHTML = spanWrap(heading.textContent);
+
+function spanWrap(word) {
+  return [...word]
+    .map((letter) => `<span>${letter}</span>`)
+    .join('');
+}
---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * JS
+ */
+
+const featured = ['Deep Dish', 'Peporoni', 'Hawaiian'];
+const speciality = ['Meatzza', 'Spicy Mama', 'Margherita'];
+const pizzas = [...featured, 'Veg', ...speciality]
+const fridayPizzas = [...pizzas];
+
+const deepDish = {
+  pizzaName: 'Deep Dish',
+  size: 'Medium',
+  ingredients: ['Marinara', 'Italin Sausage', 'Dough', 'Cheese'],
+};
