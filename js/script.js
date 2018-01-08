a88ee8fe1b42c042732fd3b1d379a9ad4bd10ca5
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-08 19:11:38 +0530

    Spread examples!

--- js/script.js
+++ js/script.js
@@ -2,13 +2,27 @@
  * JS
  */
 
-const featured = ['Deep Dish', 'Peporoni', 'Hawaiian'];
-const speciality = ['Meatzza', 'Spicy Mama', 'Margherita'];
-const pizzas = [...featured, 'Veg', ...speciality]
-const fridayPizzas = [...pizzas];
+const people = [...document.querySelectorAll('.people p')];
+const names = people.map((person) => person.textContent);
 
 const deepDish = {
   pizzaName: 'Deep Dish',
   size: 'Medium',
   ingredients: ['Marinara', 'Italin Sausage', 'Dough', 'Cheese'],
 };
+
+const shoppingList = ['Milk', 'Flour', ...deepDish.ingredients];
+console.log(shoppingList);
+
+const comments = [
+  { id: 209384, text: 'I love your dog' },
+  { id: 523423, text: 'Cuuute! 🐕'},
+  { id: 632429, text: 'You are so dumb' },
+  { id: 192834, text: 'Nice work on this Harry!' },
+];
+
+const id = 632429;
+const commentIndex = comments.findIndex(comment => comment.id === id);
+console.log(commentIndex);
+
+const newComments = [...comments.slice(0, commentIndex), ...comments.slice(commentIndex + 1)];
