cbefc093d10b0f4379cb54ad75e8ab5cc3e77518
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-08 21:26:28 +0530

    Rest Params!

--- js/script.js
+++ js/script.js
@@ -2,13 +2,18 @@
  * JS
  */
 
-const inventors = ['Einstein', 'Newton', 'Galileo'];
-const newInventors = ['Musk', 'Jobs'];
-inventors.push(...newInventors);
-
-const name = ['Harry', 'Manchanda'];
-function sayHi(first, last) {
-  alert(`Hey there ${first} ${last}`);
+function convertCurrency(rate, ...amounts) {
+  return amounts.map(amount => amount * rate);
 }
 
-sayHi(...name);
+const getAmounts = convertCurrency(64, 10, 23, 52, 1, 56);
+console.log(getAmounts);
+
+const runner = ['Harry Manchanda', 123, 5.5, 5, 3, 6, 35];
+
+const [name, id, ...runs] = runner;
+console.log(name, id, runs);
+
+const team = ['Wes', 'Kait', 'Lux', 'Sheena', 'Kelly'];
+const [captain, assistant, ...players] = team;
+console.log(captain, assistant, players)
