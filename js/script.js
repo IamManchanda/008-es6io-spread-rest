0e3f37e9904f96dc2f538354db7dd52edde573d9
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-08 20:58:40 +0530

    Spreading function!

--- js/script.js
+++ js/script.js
@@ -2,27 +2,13 @@
  * JS
  */
 
-const people = [...document.querySelectorAll('.people p')];
-const names = people.map((person) => person.textContent);
+const inventors = ['Einstein', 'Newton', 'Galileo'];
+const newInventors = ['Musk', 'Jobs'];
+inventors.push(...newInventors);
 
-const deepDish = {
-  pizzaName: 'Deep Dish',
-  size: 'Medium',
-  ingredients: ['Marinara', 'Italin Sausage', 'Dough', 'Cheese'],
-};
+const name = ['Harry', 'Manchanda'];
+function sayHi(first, last) {
+  alert(`Hey there ${first} ${last}`);
+}
 
-const shoppingList = ['Milk', 'Flour', ...deepDish.ingredients];
-console.log(shoppingList);
-
-const comments = [
-  { id: 209384, text: 'I love your dog' },
-  { id: 523423, text: 'Cuuute! 🐕'},
-  { id: 632429, text: 'You are so dumb' },
-  { id: 192834, text: 'Nice work on this Harry!' },
-];
-
-const id = 632429;
-const commentIndex = comments.findIndex(comment => comment.id === id);
-console.log(commentIndex);
-
-const newComments = [...comments.slice(0, commentIndex), ...comments.slice(commentIndex + 1)];
+sayHi(...name);
